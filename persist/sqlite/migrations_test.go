dbbe321069f79b1c40877863e6fe88abef3569da
Nate <me@n8m.us>
2025-07-29 14:29:20 -0700

    ci: add automated builds

--- persist/sqlite/migrations_test.go
+++ persist/sqlite/migrations_test.go
@@ -12,37 +12,74 @@ import (
 
 // nolint:misspell
 const initialSchema = `
-CREATE TABLE seeds (
-	id INTEGER PRIMARY KEY,
-	seed_mac BLOB UNIQUE NOT NULL CHECK(length(seed_mac) = 32),
-	encrypted_seed BLOB UNIQUE NOT NULL CHECK(length(encrypted_seed) = 72),
-	date_created INTEGER NOT NULL
+CREATE TABLE renter_metrics (
+	renter_key BLOB NOT NULL CHECK(length(renter_key) = 32), -- 32 bytes
+	date_created INTEGER NOT NULL, -- UNIX timestamp
+
+	active_contracts INTEGER NOT NULL DEFAULT 0 CHECK (active_contracts >= 0),
+	renewed_contracts INTEGER NOT NULL DEFAULT 0 CHECK (renewed_contracts >= 0),
+	successful_contracts INTEGER NOT NULL DEFAULT 0 CHECK (successful_contracts >= 0),
+	failed_contracts INTEGER NOT NULL DEFAULT 0 CHECK (failed_contracts >= 0),
+
+	active_size INTEGER NOT NULL DEFAULT 0 CHECK (active_size >= 0), -- total size of data in active contracts
+	total_size INTEGER NOT NULL DEFAULT 0 CHECK (total_size >= 0), -- total size of data in historical contracts
+
+	locked_allowance BLOB NOT NULL CHECK(length(locked_allowance) = 16), -- currency
+	spent_allowance BLOB NOT NULL CHECK(length(spent_allowance) = 16), -- currency
+
+	PRIMARY KEY (renter_key, date_created)
 );
 
-CREATE TABLE signing_keys (
-	public_key BLOB PRIMARY KEY CHECK(length(public_key) = 32),
-	seed_id INTEGER NOT NULL REFERENCES seeds (id),
-	seed_index INTEGER NOT NULL
-);
-CREATE INDEX signing_keys_seed_id_idx ON signing_keys (seed_id);
-CREATE INDEX signing_keys_seed_id_seed_index_idx ON signing_keys (seed_id, seed_index ASC);
 
-CREATE TABLE syncer_peers (
-	peer_address TEXT PRIMARY KEY NOT NULL,
-	first_seen INTEGER NOT NULL
+CREATE TABLE host_metrics (
+	host_key BLOB NOT NULL CHECK(length(host_key) = 32), -- 32 bytes
+	date_created INTEGER NOT NULL, -- UNIX timestamp
+
+	active_contracts INTEGER NOT NULL DEFAULT 0 CHECK (active_contracts >= 0),
+	renewed_contracts INTEGER NOT NULL DEFAULT 0 CHECK (renewed_contracts >= 0),
+	successful_contracts INTEGER NOT NULL DEFAULT 0 CHECK (successful_contracts >= 0),
+	failed_contracts INTEGER NOT NULL DEFAULT 0 CHECK (failed_contracts >= 0),
+
+	active_size INTEGER NOT NULL DEFAULT 0 CHECK (active_size >= 0), -- total size of data in active contracts
+	total_size INTEGER NOT NULL DEFAULT 0 CHECK (total_size >= 0), -- total size of data in historical contracts
+
+	burnt_collateral BLOB NOT NULL CHECK(length(burnt_collateral) = 16), -- currency
+	locked_collateral BLOB NOT NULL CHECK(length(locked_collateral) = 16), -- currency
+	risked_collateral BLOB NOT NULL CHECK(length(risked_collateral) = 16), -- currency
+	potential_revenue BLOB NOT NULL CHECK(length(potential_revenue) = 16), -- currency
+	earned_revenue BLOB NOT NULL CHECK(length(earned_revenue) = 16), -- currency
+
+	PRIMARY KEY (host_key, date_created)
 );
 
-CREATE TABLE syncer_bans (
-	net_cidr TEXT PRIMARY KEY NOT NULL,
-	expiration INTEGER NOT NULL,
-	reason TEXT NOT NULL
+CREATE TABLE metrics (
+	date_created INTEGER PRIMARY KEY, -- UNIX timestamp
+
+	hosts INTEGER NOT NULL DEFAULT 0,
+	renters INTEGER NOT NULL DEFAULT 0,
+
+	active_contracts INTEGER NOT NULL DEFAULT 0,
+	renewed_contracts INTEGER NOT NULL DEFAULT 0,
+	successful_contracts INTEGER NOT NULL DEFAULT 0,
+	failed_contracts INTEGER NOT NULL DEFAULT 0,
+
+	active_size INTEGER NOT NULL DEFAULT 0, -- total size of data in active contracts
+	total_size INTEGER NOT NULL DEFAULT 0, -- total size of data in historical contracts
+
+	potential_revenue BLOB NOT NULL CHECK(length(potential_revenue) = 16), -- currency
+	earned_revenue BLOB NOT NULL CHECK(length(earned_revenue) = 16), -- currency
+	locked_collateral BLOB NOT NULL CHECK(length(locked_collateral) = 16), -- currency
+	risked_collateral BLOB NOT NULL CHECK(length(risked_collateral) = 16), -- currency
+	burnt_collateral BLOB NOT NULL CHECK(length(burnt_collateral) = 16), -- currency
+
+	locked_allowance BLOB NOT NULL CHECK(length(locked_allowance) = 16), -- currency
+	spent_allowance BLOB NOT NULL CHECK(length(spent_allowance) = 16) -- currency
 );
-CREATE INDEX syncer_bans_expiration_idx ON syncer_bans (expiration);
 
 CREATE TABLE global_settings (
 	id INTEGER PRIMARY KEY NOT NULL DEFAULT 0 CHECK (id = 0), -- enforce a single row
 	db_version INTEGER NOT NULL, -- used for migrations
-	key_salt BLOB -- the salt used for deriving keys
+	last_index BLOB CHECK(length(last_index) = 40) -- 32 + 8
 );`
 
 func TestMigrationConsistency(t *testing.T) {
